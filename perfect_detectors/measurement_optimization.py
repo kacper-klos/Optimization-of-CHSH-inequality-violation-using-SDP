5ebb5f8c11d92bcbc4522ffa3c2e79bf1981faa6
kacper16621 <kacper.klos@proton.me>
2025-08-11 19:27:24 +0200

    Asserting proper input matrix shape for every function

--- perfect_detectors/measurement_optimization.py
+++ perfect_detectors/measurement_optimization.py
@@ -3,9 +3,13 @@ import numpy.typing as npt
 import cvxpy as cp
 
 def A_partial_trace(M: np.ndarray) -> np.ndarray:
+    # Assert proper input shape
+    assert(M.shape == (4, 4))
     return (M[:2, :2] + M[2:, 2:]).copy()
 
 def B_partial_trace(M: np.ndarray) -> np.ndarray:
+    # Assert proper input shape
+    assert(M.shape == (4, 4))
     return np.array(
         [[np.trace(M[:2, :2]), np.trace(M[:2, 2:])],
          [np.trace(M[2:, :2]), np.trace(M[2:, 2:])]]
--- perfect_detectors/state_optimization.py
+++ perfect_detectors/state_optimization.py
@@ -8,6 +8,12 @@ def chsh_state_optimization(A1: npt.NDArray[np.complex128],
                             A2: npt.NDArray[np.complex128], 
                             B1: npt.NDArray[np.complex128], 
                             B2: npt.NDArray[np.complex128]) -> Tuple[np.float64, npt.NDArray[np.complex128]]:
+    # Assert proper input sizes
+    assert(A1.shape == (2, 2))
+    assert(A2.shape == (2, 2))
+    assert(B1.shape == (2, 2))
+    assert(B2.shape == (2, 2))
+
     # CHSH operator
     CHSH = np.kron(A1, B1 + B2) + np.kron(A2, B1 - B2)
 
