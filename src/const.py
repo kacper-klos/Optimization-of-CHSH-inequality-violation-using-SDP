11ac5fdc8453bee32e99bca680dbdfa108777ed9
kacper16621 <kacper.klos@proton.me>
2025-08-13 17:18:26 +0200

    Function description for constants and state code

--- src/const.py
+++ src/const.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 # Pauli matrices
 I = np.eye(2, dtype=np.complex128)
 X = np.array([[0, 1], [1, 0]], dtype=np.complex128)
@@ -18,18 +17,32 @@ THRESHOLD = 1e-8
 ZERO_KET = np.array([1, 0])
 ERROR_STATE = np.outer(np.kron(ZERO_KET, ZERO_KET), np.kron(ZERO_KET, ZERO_KET))
 
-# Make random valid measurement matrix
-def random_measurement():
+def random_measurement() -> np.ndarray:
+    """ Returns random matrix which is valid measurement setting.
+
+    Returns:
+        Random measurement.
+    """
     v = np.random.normal(size = 3)
     v /= np.linalg.norm(v)
     return X*v[0] + Y *v[1] + Z * v[2]
 
-def array_pretty(A: np.ndarray) -> np.ndarray:
+def array_pretty(A: np.ndarray, threshold = THRESHOLD) -> np.ndarray:
+    """ Set zero to every value in array copy below threshold.
+
+    Args:
+        A: array which values will be set to zero.
+        threshold: value under which values will be set to zero
+
+    Returns:
+        Copy with values set to zero.
+    """
+
     A_copy = A.copy()
-    
+    # Zero real values
     small_real = np.abs(A_copy.real) < THRESHOLD
     A_copy.real[small_real] = 0
-    
+    # Zero imaginary values
     small_imag = np.abs(A_copy.imag) < THRESHOLD
     A_copy.imag[small_imag] = 0
     
--- src/state_optimization.py
+++ src/state_optimization.py
@@ -2,19 +2,36 @@ import numpy as np
 import cvxpy as cp
 from typing import Tuple
 
+from const import ERROR_STATE
+
 def chsh_state_optimization(A1: np.ndarray, 
                             A2: np.ndarray, 
                             B1: np.ndarray, 
                             B2: np.ndarray,
                             mu: float = 1.0,
-                            error_state: np.ndarray = np.zeros((4,4))) -> Tuple[np.float64, np.ndarray]:
+                            error_state: np.ndarray = ERROR_STATE) -> Tuple[np.float64, np.ndarray]:
+    """ Finds the most optimal quantum state for maximum CHSH inequalit violation with imperfect measuring device.
+
+    Args:
+        A1: Alice first measurement setting, must be 2x2 array.
+        A2: Alice second measurement setting, must be 2x2 array.
+        B1: Bob first measurement setting, must be 2x2 array.
+        B2: Bob second measurement setting, must be 2x2 array.
+        mu: Efficiency of the detector, must be in range [0; 1].
+        error_state: Density matrix of a state which appears when detector fails.
+
+    Returns:
+        Tuple[np.float64, np.ndarray]: CHSH value and the state which cause it, in this order
+    """
 
     # Assert proper input sizes
     assert(A1.shape == (2, 2))
     assert(A2.shape == (2, 2))
     assert(B1.shape == (2, 2))
     assert(B2.shape == (2, 2))
+    # Assert proper density matrix
     assert(error_state.shape == (4, 4))
+    assert(np.linalg.trace(error_state) == 1)
     # Assert proper fraction
     assert(mu >= 0 and mu <= 1)
 
